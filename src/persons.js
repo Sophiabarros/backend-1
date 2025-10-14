b291209a5029b2152c42a03a78864b57e5a9ee04
sophiabarros <soohbarros1@gmail.com>
2025-10-13 21:06:52 -0300

    Commit final do backend

---
+++ src/persons.js
@@ -0,0 +1,31 @@
+const persons = [
+   {
+    name: "Guilherme Carvalho",
+    nickname: "nextage bb",
+    age: 16,
+    email: "guilhermecarvalho@gmail.com"
+   }, 
+
+    {
+    name: "Victor Fustte",
+    nickname: "vg",
+    age: 12,
+    email: "vg_fustte@gmail.com"
+   }, 
+
+    {
+    name: "Sophia Leal",
+    nickname: "Garota confusa",
+    age: 20,
+    email: "sophialeal@gmail.com"
+   }, 
+
+    {
+    name: "Laura Rodrigues",
+    nickname: "Laura + ego",
+    age: 17,
+    email: "lauraamavictor@gmail.com"
+   }, 
+]
+
+export{ persons }
